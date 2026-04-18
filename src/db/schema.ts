27826f26999c5e34b1c939aaa229c8b3dd07a573
Humberto Yupanqui Apaza <yupanquiah@gmail.com>
2026-04-18 17:53:32 -0500

    feat: implement authentication schema and update auth client configuration

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,7 +1,2 @@
-import { pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
-
-export const todos = pgTable("todos", {
-  id: serial().primaryKey(),
-  title: text().notNull(),
-  createdAt: timestamp("created_at").defaultNow(),
-});
+export * from "@/db/schema/auth";
+export * from "@/db/schema/user";
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,12 @@
 import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { tanstackStartCookies } from "better-auth/tanstack-start";
+import { db } from "@/db";
 
 export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+  }),
   emailAndPassword: {
     enabled: true,
   },
